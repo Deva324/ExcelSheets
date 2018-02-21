3c3e74c68f453ee7c3f6349eddbedb7bc9012b6b
dgadiam <dgadiam@USHYDDGADIAM1.us.deloitte.com>
2018-02-21 19:54:01 +0530

    Rows copy with formula

--- src/main/java/com/excel/template/ReadExcel.java
+++ src/main/java/com/excel/template/ReadExcel.java
@@ -4,17 +4,24 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
 
 import org.apache.commons.io.FileUtils;
-import org.apache.poi.xssf.usermodel.XSSFChart;
-import org.apache.poi.xssf.usermodel.XSSFDrawing;
+import org.apache.poi.ss.usermodel.CellCopyPolicy;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openxmlformats.schemas.drawingml.x2006.chart.CTBarChart;
 import org.openxmlformats.schemas.drawingml.x2006.chart.CTBarSer;
+import org.openxmlformats.schemas.drawingml.x2006.spreadsheetDrawing.CTMarker;
+import org.openxmlformats.schemas.drawingml.x2006.spreadsheetDrawing.CTTwoCellAnchor;
 
 public class ReadExcel {
 	private static final String fileName = "C:\\Users\\dgadiam\\Documents\\";
@@ -23,18 +30,30 @@ public class ReadExcel {
 
 		String name = "Sprint-StoryTracker-Template-Input.xlsx";
 
-		Map<String, String> stories = readFile(name);
+		Map<String, String> stories = ExcelUtil.readFile(name);
 		generateExcel(stories);
 
 	}
 
 	private static void generateExcel(Map<String, String> stories) {
 		try {
-			FileUtils.copyFile(new File(fileName + "Sprint-StoryTracker-Template.xlsx"),
-					new File(fileName + "Sprint-StoryTracker-Template1.xlsx"));
-			FileInputStream fis = new FileInputStream(fileName + "Sprint-StoryTracker-Template1.xlsx");
+			FileUtils.copyFile(new File(fileName + "Sprint-StoryTracker-Template-dup.xlsx"),
+					new File(fileName + "Sprint-StoryTracker-Template2.xlsx"));
+			FileInputStream fis = new FileInputStream(fileName + "Sprint-StoryTracker-Template2.xlsx");
 			XSSFWorkbook workbook = new XSSFWorkbook(fis);
-			XSSFSheet s = workbook.getSheet("StoryID");
+			XSSFSheet s = workbook.getSheet("Overview");
+			CellCopyPolicy cp = new CellCopyPolicy();
+			for (Map.Entry<String, String> entry : stories.entrySet()) {
+				int n = s.getLastRowNum();
+				XSSFRow srcRow = s.getRow(n);
+				XSSFRow newRow = s.createRow(n + 1);
+				s.copyRows(n, n, n + 1, cp);
+				newRow = s.getRow(n + 1);
+				newRow.getCell(0).setCellValue(entry.getKey());
+				newRow.getCell(1).setCellValue(entry.getValue());
+				ExcelUtil.copyRow(srcRow, newRow);
+			}
+
 			/*
 			 * XSSFDrawing d = s.getDrawingPatriarch(); List<XSSFChart> l = d.getCharts();
 			 * for (int i = 0; i < l.size(); i++) { XSSFChart c = l.get(i); //
@@ -49,30 +68,22 @@ public class ReadExcel {
 			 * 
 			 * }
 			 */
-			XSSFDrawing d1 = workbook.cloneSheet(1, "Test").createDrawingPatriarch();
-			List<XSSFChart> l1 = d1.getCharts();
-			for (int i = 0; i < l1.size(); i++) {
-				XSSFChart c = l1.get(i);
-				List<CTBarChart> c1 = c.getCTChart().getPlotArea().getBarChartList();
-				for (CTBarChart c2 : c1) {
-					List<CTBarSer> s2 = c2.getSerList();
-					for (CTBarSer s3 : s2) {
-						String s4=s3.getTx().getStrRef().getF();
-						s4=s4.replace("StoryID", "Test");
-						s3.getTx().getStrRef().setF(s4);
-						String s5=s3.getVal().getNumRef().getF();
-						s5=s5.replace("StoryID", "Test");
-						s3.getVal().getNumRef().setF(s5);
-						//System.out.println(s4);
-						//System.out.println(s5);
-						//System.out.println(s3.getTx().getStrRef().getF());
-						//System.out.println(s3.getVal().getNumRef().getF());
-
-					}
-				}
-				System.out.println(l1.get(i).getCTChart());
-			}
-			FileOutputStream fileOut = new FileOutputStream(new File(fileName + "Sprint-StoryTracker-Template1.xlsx"));
+			/*
+			 * XSSFDrawing d1 = workbook.cloneSheet(1, "Test").createDrawingPatriarch();
+			 * List<XSSFChart> l1 = d1.getCharts(); for (int i = 0; i < l1.size(); i++) {
+			 * XSSFChart c = l1.get(i); List<CTBarChart> c1 =
+			 * c.getCTChart().getPlotArea().getBarChartList(); for (CTBarChart c2 : c1) {
+			 * List<CTBarSer> s2 = c2.getSerList(); for (CTBarSer s3 : s2) { String
+			 * s4=s3.getTx().getStrRef().getF(); s4=s4.replace("StoryID", "Test");
+			 * s3.getTx().getStrRef().setF(s4); String s5=s3.getVal().getNumRef().getF();
+			 * s5=s5.replace("StoryID", "Test"); s3.getVal().getNumRef().setF(s5);
+			 * //System.out.println(s4); //System.out.println(s5);
+			 * //System.out.println(s3.getTx().getStrRef().getF());
+			 * //System.out.println(s3.getVal().getNumRef().getF());
+			 * 
+			 * } } System.out.println(l1.get(i).getCTChart()); }
+			 */
+			FileOutputStream fileOut = new FileOutputStream(new File(fileName + "Sprint-StoryTracker-Template2.xlsx"));
 			workbook.write(fileOut);
 			workbook.close();
 			fileOut.close();
@@ -81,25 +92,6 @@ public class ReadExcel {
 			e.printStackTrace();
 		}
 
-	}
-
-	private static Map<String, String> readFile(String name) {
-		Map<String, String> stories = new HashMap<String, String>();
-		try (FileInputStream fis = new FileInputStream(fileName + "Sprint-StoryTracker-Template-Input.xlsx")) {
-			XSSFWorkbook workbook = new XSSFWorkbook(fis);
-			XSSFSheet s = workbook.getSheetAt(0);
-			int noOfRows = s.getLastRowNum();
-			for (int i = 1; i <= noOfRows; i++) {
-				String storyNumber = s.getRow(i).getCell(0).getStringCellValue();
-				String storyDescription = s.getRow(i).getCell(1).getStringCellValue();
-				stories.put(storyNumber, storyDescription);
-			}
-
-			workbook.close();
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-		}
-		return stories;
 	}
 
 }
---
+++ src/main/java/com/excel/template/RowCopyBack.java
@@ -0,0 +1,210 @@
+package com.excel.template;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.DataFormat;
+import org.apache.poi.ss.usermodel.Font;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+
+public class RowCopyBack {
+	public static void copyRow(XSSFRow srcRow, XSSFRow newRow) {
+		List<CellStyle> styleMap = new ArrayList<>();
+		newRow.setHeight(srcRow.getHeight());
+		int j = srcRow.getFirstCellNum();
+		if (j < 0) {
+			j = 0;
+		}
+		for (; j <= srcRow.getLastCellNum(); j++) {
+			XSSFCell oldCell = srcRow.getCell(j);
+			XSSFCell newCell = newRow.getCell(j);
+			if (oldCell != null) {
+				if (newCell == null) {
+					newCell = newRow.createCell(j);
+				}
+				copyCell(oldCell, newCell, styleMap);
+			}
+		}
+
+	}
+
+	private static void copyCell(Cell oldCell, Cell newCell, List<CellStyle> styleList) {
+		if (styleList != null) {
+			if (oldCell.getSheet().getWorkbook() == newCell.getSheet().getWorkbook()) {
+				newCell.setCellStyle(oldCell.getCellStyle());
+			} else {
+				DataFormat newDataFormat = newCell.getSheet().getWorkbook().createDataFormat();
+
+				CellStyle newCellStyle = getSameCellStyle(oldCell, newCell, styleList);
+				if (newCellStyle == null) {
+					Font oldFont = oldCell.getSheet().getWorkbook().getFontAt(oldCell.getCellStyle().getFontIndex());
+					Font newFont = newCell.getSheet().getWorkbook().findFont(oldFont.getBold(), oldFont.getColor(),
+							oldFont.getFontHeight(), oldFont.getFontName(), oldFont.getItalic(), oldFont.getStrikeout(),
+							oldFont.getTypeOffset(), oldFont.getUnderline());
+					if (newFont == null) {
+						newFont = newCell.getSheet().getWorkbook().createFont();
+						newFont.setBold(oldFont.getBold());
+						newFont.setColor(oldFont.getColor());
+						newFont.setFontHeight(oldFont.getFontHeight());
+						newFont.setFontName(oldFont.getFontName());
+						newFont.setItalic(oldFont.getItalic());
+						newFont.setStrikeout(oldFont.getStrikeout());
+						newFont.setTypeOffset(oldFont.getTypeOffset());
+						newFont.setUnderline(oldFont.getUnderline());
+						newFont.setCharSet(oldFont.getCharSet());
+					}
+
+					short newFormat = newDataFormat.getFormat(oldCell.getCellStyle().getDataFormatString());
+					newCellStyle = newCell.getSheet().getWorkbook().createCellStyle();
+					newCellStyle.setFont(newFont);
+					newCellStyle.setDataFormat(newFormat);
+					newCellStyle.setHidden(oldCell.getCellStyle().getHidden());
+					newCellStyle.setLocked(oldCell.getCellStyle().getLocked());
+					newCellStyle.setWrapText(oldCell.getCellStyle().getWrapText());
+					newCellStyle.setBottomBorderColor(oldCell.getCellStyle().getBottomBorderColor());
+					newCellStyle.setFillBackgroundColor(oldCell.getCellStyle().getFillBackgroundColor());
+					newCellStyle.setFillForegroundColor(oldCell.getCellStyle().getFillForegroundColor());
+					newCellStyle.setIndention(oldCell.getCellStyle().getIndention());
+					newCellStyle.setLeftBorderColor(oldCell.getCellStyle().getLeftBorderColor());
+					newCellStyle.setRightBorderColor(oldCell.getCellStyle().getRightBorderColor());
+					newCellStyle.setRotation(oldCell.getCellStyle().getRotation());
+					newCellStyle.setTopBorderColor(oldCell.getCellStyle().getTopBorderColor());
+					styleList.add(newCellStyle);
+				}
+				newCell.setCellStyle(newCellStyle);
+			}
+		}
+		switch (oldCell.getCellType()) {
+		case Cell.CELL_TYPE_STRING:
+			newCell.setCellValue(oldCell.getStringCellValue());
+			break;
+		case Cell.CELL_TYPE_NUMERIC:
+			newCell.setCellValue(oldCell.getNumericCellValue());
+			break;
+		case Cell.CELL_TYPE_BLANK:
+			newCell.setCellType(Cell.CELL_TYPE_BLANK);
+			break;
+		case Cell.CELL_TYPE_BOOLEAN:
+			newCell.setCellValue(oldCell.getBooleanCellValue());
+			break;
+		case Cell.CELL_TYPE_ERROR:
+			newCell.setCellErrorValue(oldCell.getErrorCellValue());
+			break;
+		case Cell.CELL_TYPE_FORMULA:
+			newCell.setCellFormula(oldCell.getCellFormula());
+
+			break;
+		default:
+			break;
+		}
+	}
+
+	private static CellStyle getSameCellStyle(Cell oldCell, Cell newCell, List<CellStyle> styleList) {
+		CellStyle styleToFind = oldCell.getCellStyle();
+		CellStyle currentCellStyle = null;
+		CellStyle returnCellStyle = null;
+		Iterator<CellStyle> iterator = styleList.iterator();
+		Font oldFont = null;
+		Font newFont = null;
+		while (iterator.hasNext() && returnCellStyle == null) {
+			currentCellStyle = iterator.next();
+
+			if (currentCellStyle.getAlignment() != styleToFind.getAlignment()) {
+				continue;
+			}
+			if (currentCellStyle.getHidden() != styleToFind.getHidden()) {
+				continue;
+			}
+			if (currentCellStyle.getLocked() != styleToFind.getLocked()) {
+				continue;
+			}
+			if (currentCellStyle.getWrapText() != styleToFind.getWrapText()) {
+				continue;
+			}
+			if (currentCellStyle.getBorderBottom() != styleToFind.getBorderBottom()) {
+				continue;
+			}
+			if (currentCellStyle.getBorderLeft() != styleToFind.getBorderLeft()) {
+				continue;
+			}
+			if (currentCellStyle.getBorderRight() != styleToFind.getBorderRight()) {
+				continue;
+			}
+			if (currentCellStyle.getBorderTop() != styleToFind.getBorderTop()) {
+				continue;
+			}
+			if (currentCellStyle.getBottomBorderColor() != styleToFind.getBottomBorderColor()) {
+				continue;
+			}
+			if (currentCellStyle.getFillBackgroundColor() != styleToFind.getFillBackgroundColor()) {
+				continue;
+			}
+			if (currentCellStyle.getFillForegroundColor() != styleToFind.getFillForegroundColor()) {
+				continue;
+			}
+			if (currentCellStyle.getFillPattern() != styleToFind.getFillPattern()) {
+				continue;
+			}
+			if (currentCellStyle.getIndention() != styleToFind.getIndention()) {
+				continue;
+			}
+			if (currentCellStyle.getLeftBorderColor() != styleToFind.getLeftBorderColor()) {
+				continue;
+			}
+			if (currentCellStyle.getRightBorderColor() != styleToFind.getRightBorderColor()) {
+				continue;
+			}
+			if (currentCellStyle.getRotation() != styleToFind.getRotation()) {
+				continue;
+			}
+			if (currentCellStyle.getTopBorderColor() != styleToFind.getTopBorderColor()) {
+				continue;
+			}
+			if (currentCellStyle.getVerticalAlignment() != styleToFind.getVerticalAlignment()) {
+				continue;
+			}
+
+			oldFont = oldCell.getSheet().getWorkbook().getFontAt(oldCell.getCellStyle().getFontIndex());
+			newFont = newCell.getSheet().getWorkbook().getFontAt(currentCellStyle.getFontIndex());
+
+			if (newFont.getBold() == oldFont.getBold()) {
+				continue;
+			}
+			if (newFont.getColor() == oldFont.getColor()) {
+				continue;
+			}
+			if (newFont.getFontHeight() == oldFont.getFontHeight()) {
+				continue;
+			}
+			if (newFont.getFontName() == oldFont.getFontName()) {
+				continue;
+			}
+			if (newFont.getItalic() == oldFont.getItalic()) {
+				continue;
+			}
+			if (newFont.getStrikeout() == oldFont.getStrikeout()) {
+				continue;
+			}
+			if (newFont.getTypeOffset() == oldFont.getTypeOffset()) {
+				continue;
+			}
+			if (newFont.getUnderline() == oldFont.getUnderline()) {
+				continue;
+			}
+			if (newFont.getCharSet() == oldFont.getCharSet()) {
+				continue;
+			}
+			if (oldCell.getCellStyle().getDataFormatString().equals(currentCellStyle.getDataFormatString())) {
+				continue;
+			}
+
+			returnCellStyle = currentCellStyle;
+		}
+		return returnCellStyle;
+	}
+
+}
